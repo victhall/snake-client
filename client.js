01cd8097b46c33492e02174eb3dfcc03e650579c
“Victoria <victoriahall.dev@gmail.com>
2021-12-02 02:19:26 +0000

    completed file

--- client.js
+++ client.js
@@ -1,29 +1,29 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
 
 // establishes a connection with the game server
 const connect = function () {
+
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT,
   });
-  
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write("Name: Vic");
-    setInterval(() => {
-     conn.write('Move: up'); 
-    }, 200)
-    
+
+
+    conn.on('data', (data) => {
+      console.log('Server says: ', data);
+
+    });
+
+    return conn;
+
   });
-
-  return conn;
-};
-
+}
 module.exports = connect;
